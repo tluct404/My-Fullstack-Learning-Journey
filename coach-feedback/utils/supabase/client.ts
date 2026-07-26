a4e36edbf8d7e275cad8895afdad3f7beafebd19
tluct404 <timothylucastong@gmail.com>
2026-07-26 22:01:04 +1000

    client.ts

--- coach-feedback/utils/supabase/client.ts
+++ coach-feedback/utils/supabase/client.ts
@@ -1,8 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const createClient = () =>
+  createBrowserClient(
+    supabaseUrl!,
+    supabaseKey!,
   );
-}
